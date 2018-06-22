f2b6d3aa547a98cb665208e95f0b7f18f0fe121b
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-22 12:39:35 +0900

    posts_list 연결

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
+
+from posts.models import Post
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(requset):
+    # post = Post.objects.all()
+    # context = {
+    #     'posts':post,
+    # }
+    #
+    # return render(requset, 'post-list', context)
+    return HttpResponse('post-list')
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        'posts':post,
+        'pk':pk,
+    }
+
+    return render(request, 'post-detail', context)
