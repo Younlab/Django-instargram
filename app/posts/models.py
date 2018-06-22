aa3bf0a5fbf4f7543b1438a2701acd57dc28a5ac
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-22 11:06:09 +0900

    posts model create

--- app/posts/models.py
+++ app/posts/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    photo = models.ImageField()
-    content = models.TextField()
+    photo = models.ImageField(upload_to='post', blank=True)
+    content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
